7a5bb8017cc16fde8438abea219545742840f3ff
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-06-26 18:46:42 +0100

    feat: numerical integration

--- integration.py
+++ integration.py
@@ -18,6 +18,46 @@ def trapezoidal_rule(f, a, b, n):
     dx = (b - a) / n
     return (f(a) + 2 * sum(f(a + i * dx) for i in range(1, n)) + f(b)) * dx / 2
 
+
+def trapezoidal_rule(f, a, b, N):
+    h = (b - a) / N
+    s = 0.5 * f(a) + 0.5 * f(b) + sum(f(a + i * h) for i in range(1, N))
+    return s * h
+
+def romberg_integration(f, a, b, tol=1e-6, max_iter=1000):
+    R = [[0]]
+    R[0][0] = trapezoidal_rule(f, a, b, 1)
+    n = 1
+
+    while n < max_iter:
+        h = (b - a) / 2.0 ** n
+        R.append([0] * (n + 1))
+        # Compute trapezoidal rule for next level of subdivision
+        R[n][0] = 0.5 * R[n - 1][0] + h * sum(f(a + (2 * k - 1) * h) for k in range(1, 2 ** (n - 1) + 1))
+
+        # Compute extrapolation estimates
+        for m in range(1, n + 1):
+            R[n][m] = R[n][m - 1] + 1.0 / (4 ** m - 1) * (R[n][m - 1] - R[n - 1][m - 1])
+
+        # Check if the last two estimates are close enough
+        if abs(R[n][n - 1] - R[n][n]) < tol:
+            return R[n][n]
+
+        n += 1
+
+    raise Exception(f"Romberg integration did not converge within {max_iter} iterations")
+
+# example usage
+def f(x):
+    return x**2
+
+result = romberg_integration(f, 0, 1)
+print(result)
+
+def riemann_sum_midpoint(f, a, b, n):
+    dx = (b - a) / n
+    return sum(f(a + dx * (i + 0.5)) for i in range(n)) * dx
+
 def adaptive_quadrature(f, a, b, tol=1e-6):
     # Simpson's rule
     h = b - a
