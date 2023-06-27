5ede23f7a164188d71165b18e88027c425be1c0b
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-06-27 12:14:56 +0100

    feat: importance sampling for outliers

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # random_code
-just to store code and then move it to other places
+
+# Importance sampling for outliers
+
+Unlike bootstrap resampling, importance sampling is not typically used directly for outlier detection. Rather, it is used to improve the efficiency of statistical estimation procedures, particularly in situations where the quantity of interest is rare or the sampling from the target distribution is difficult.
+
+In the context of outlier detection, importance sampling might be useful in scenarios where the data is heavily imbalanced, and outliers represent a rare event. However, it's worth noting that this would not be the standard or most straightforward approach to outlier detection, as it requires a careful selection of the proposal distribution and might be more computationally intensive than other methods.
+
+In this example, we first generate a synthetic dataset data that is mostly composed of samples from a normal distribution with mean 0 and standard deviation 1, but also includes some outliers from a normal distribution with mean 5 and standard deviation 0.5.
+
+We then define the target and proposal distributions based on the data and use the outlier_detection_importance_sampling function to estimate the proportion of outliers. The outliers are defined as data points that are more than three standard deviations away from the mean.
+
+Please note that the proposal distribution is chosen to be wider than the target distribution to improve the chances of sampling the rare outliers, and the number of samples N is set to a large number (10,000) to get a reliable estimate. The choice of the threshold for outlier detection is often problem-specific and may require some experimentation.
---
+++ importance_sampling_outliers.py
@@ -0,0 +1,66 @@
+# Unlike bootstrap resampling, importance sampling is not typically used directly for outlier detection. Rather, it is used to improve the efficiency of statistical estimation procedures, particularly in situations where the quantity of interest is rare or the sampling from the target distribution is difficult.
+
+# In the context of outlier detection, importance sampling might be useful in scenarios where the data is heavily imbalanced, and outliers represent a rare event. However, it's worth noting that this would not be the standard or most straightforward approach to outlier detection, as it requires a careful selection of the proposal distribution and might be more computationally intensive than other methods.
+
+# Here's a conceptual example where we try to estimate the proportion of data points that fall beyond a certain threshold using importance sampling:
+
+
+from typing import Callable
+import numpy as np
+import scipy.stats as stats
+
+def outlier_detection_importance_sampling(data: np.ndarray, threshold: float, target_dist: Callable[[float], float], proposal_dist: Callable[[float], float], proposal_sampler: Callable[[], float], N: int) -> float:
+    """
+    Estimate the proportion of outliers in a data set using importance sampling.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The data set.
+    threshold : float
+        The threshold for outlier detection.
+    target_dist : Callable[[float], float]
+        The target distribution.
+    proposal_dist : Callable[[float], float]
+        The proposal distribution.
+    proposal_sampler : Callable[[], float]
+        The function to draw samples from the proposal distribution.
+    N : int
+        The number of samples to draw.
+
+    Returns
+    -------
+    float
+        The estimated proportion of outliers.
+    """
+
+    # Define the indicator function for outliers
+    def outlier_indicator(x):
+        return 1 if x > threshold else 0
+
+    # Draw samples from the proposal distribution
+    samples = np.array([proposal_sampler() for _ in range(N)])
+
+    # Compute the weights for each sample
+    weights = target_dist(samples) / proposal_dist(samples)
+
+    # Compute the weighted average of the outlier indicator function
+    outlier_estimate = np.sum(outlier_indicator(samples) * weights) / np.sum(weights)
+
+    return outlier_estimate
+
+# Generate some synthetic data
+np.random.seed(42)  # for reproducibility
+data = np.concatenate([np.random.normal(loc=0, scale=1, size=10000), np.random.normal(loc=5, scale=0.5, size=100)])
+
+# Define the target distribution as a standard normal distribution
+target_dist = stats.norm(loc=np.mean(data), scale=np.std(data)).pdf
+
+# Define the proposal distribution as a wider normal distribution
+proposal_dist = stats.norm(loc=np.mean(data), scale=2*np.std(data)).pdf
+
+# Define the function to draw samples from the proposal distribution
+proposal_sampler = stats.norm(loc=np.mean(data), scale=2*np.std(data)).rvs
+
+# Estimate the proportion of outliers using importance sampling
+print("Estimated proportion of outliers: ", outlier_detection_importance_sampling(data, threshold=3*np.std(data), target_dist=target_dist, proposal_dist=proposal_dist, proposal_sampler=proposal_sampler, N=10000))
