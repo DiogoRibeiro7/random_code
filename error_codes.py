075590cb517f82be8e9b20317f92ef33cfd66dc6
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-07-05 20:54:02 +0100

    feat: custom errors

--- error_codes.py
+++ error_codes.py
@@ -26,3 +26,43 @@ class CustomAttributeError(AttributeError):
         self.attribute_name = attribute_name
         self.message = f"Attribute '{attribute_name}' does not exist for object '{object_name}'"
         super().__init__(self.message)
+
+
+
+
+
+
+
+
+
+
+try:
+    # Code that may raise custom errors or general exceptions
+    # ...
+
+    # Example code that may raise custom errors
+    column_name = 'Salary'
+    if column_name not in df.columns:
+        raise CustomError1(f"Missing column: {column_name}")
+    elif df[column_name].dtype != int:
+        raise CustomError2(f"Invalid data type in column '{column_name}'. Expected type: int, Actual type: {df[column_name].dtype}")
+
+    # Other code statements...
+    # ...
+
+except CustomError1 as e:
+    # Handle CustomError1
+    print(f"Custom Error 1: {e.message}")
+
+except CustomError2 as e:
+    # Handle CustomError2
+    print(f"Custom Error 2: {e.message}")
+
+except Exception as e:
+    # Handle other general exceptions or re-raise if not a custom error
+    if isinstance(e, CustomError):
+        # Log or handle the custom error here
+        print("Custom Error occurred")
+    else:
+        # Re-raise the general exception
+        raise e
