fc4d6e4e98e67cdc1b75f599bd46915c8c60304d
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-07-06 21:31:15 +0100

    feat: mcmc

--- mcmc_sampling.py
+++ mcmc_sampling.py
@@ -70,23 +70,62 @@ class Sampler:
 
         return np.array(posterior)
 
-    def plot_proposal(self, mu_current: float, mu_proposal: float, accepted: bool, trace: List[float], i: int) -> None:
-        """
-        Plot the proposal at the current step.
+def plot_proposal(self, mu_current: float, mu_proposal: float, accepted: bool, trace: List[float], i: int) -> None:
+    """
+    Plot the proposal at the current step.
 
-        Args:
-            mu_current: current mu value.
-            mu_proposal: proposed mu value.
-            accepted: whether the proposal was accepted.
-            trace: trace of the mu values.
-            i: current step.
-        """
-        trace = copy(trace)
-        fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(16, 4))
-        fig.suptitle('Iteration %i' % (i + 1))
-        x = np.linspace(-3, 3, 5000)
-        color = 'g' if accepted else 'r'
+    Args:
+        mu_current: current mu value.
+        mu_proposal: proposed mu value.
+        accepted: whether the proposal was accepted.
+        trace: trace of the mu values.
+        i: current step.
+    """
+    trace = copy(trace)
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(16, 4))
+    fig.suptitle('Iteration %i' % (i + 1))
+    x = np.linspace(-3, 3, 5000)
+    color = 'g' if accepted else 'r'
 
-        # Omitted plot details for brevity
+    # Plot prior
+    prior_current = norm(self.mu_prior_mu, self.mu_prior_sd).pdf(mu_current)
+    prior_proposal = norm(self.mu_prior_mu, self.mu_prior_sd).pdf(mu_proposal)
+    prior = norm(self.mu_prior_mu, self.mu_prior_sd).pdf(x)
+    ax1.plot(x, prior)
+    ax1.plot([mu_current] * 2, [0, prior_current], marker='o', color='b')
+    ax1.plot([mu_proposal] * 2, [0, prior_proposal], marker='o', color=color)
+    ax1.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax1.set(ylabel='Probability Density', title='current: prior(mu=%.2f) = %.2f\nproposal: prior(mu=%.2f) = %.2f' % (mu_current, prior_current, mu_proposal, prior_proposal))
 
-        plt.tight_layout()
+    # Likelihood
+    likelihood_current = norm(mu_current, 1).pdf(self.data).prod()
+    likelihood_proposal = norm(mu_proposal, 1).pdf(self.data).prod()
+    y = norm(loc=mu_proposal, scale=1).pdf(x)
+    sns.distplot(self.data, kde=False, norm_hist=True, ax=ax2)
+    ax2.plot(x, y, color=color)
+    ax2.axvline(mu_current, color='b', linestyle='--', label='mu_current')
+    ax2.axvline(mu_proposal, color=color, linestyle='--', label='mu_proposal')
+    ax2.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax2.set(title='likelihood(mu=%.2f) = %.2f\nlikelihood(mu=%.2f) = %.2f' % (mu_current, 1e14*likelihood_current, mu_proposal, 1e14*likelihood_proposal))
+
+    # Posterior
+    posterior_current = self.calc_posterior_analytical(mu_current)
+    posterior_proposal = self.calc_posterior_analytical(mu_proposal)
+    ax3.plot(x, posterior_current)
+    ax3.plot([mu_current] * 2, [0, posterior_current], marker='o', color='b')
+    ax3.plot([mu_proposal] * 2, [0, posterior_proposal], marker='o', color=color)
+    ax3.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax3.set(title='posterior(mu=%.2f) = %.5f\nposterior(mu=%.2f) = %.5f' % (mu_current, posterior_current, mu_proposal, posterior_proposal))
+
+    # Trace
+    if accepted:
+        trace.append(mu_proposal)
+    else:
+        trace.append(mu_current)
+    ax4.plot(trace)
+    ax4.set(xlabel='iteration', ylabel='mu', title='trace')
+    plt.tight_layout()
+
